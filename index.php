dfb04fe9b4d8018d71ee68267e00d47e745aa9ba
Grégory Jonville <gregory.jonville@laplateforme.io>
2021-01-18 07:58:07 +0100

    site perso

---
+++ index.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="Bienvenue dans mon monde ! à la recherche active d'une alternance pour Mars 2021" /> <!-- Meta Description -->
+        <meta name="author" content="Grégory JONVILLE" /> <!-- Meta Auteur -->
+        <title>Mon Portfolio</title>
+        <link rel="icon" type="image/x-icon" href="assets/img/profile.jpg" />
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v5.15.1/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet" type="text/css" />
+        <!-- CSS-->
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation -->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-info fixed-top" id="sideNav">
+            <a class="navbar-brand js-scroll-trigger" href="#page-top">
+                <span class="d-block d-lg-none">Grégory Jonville</span>
+                <span class="d-none d-lg-block"><img class="img-fluid img-profile rounded-circle mx-auto mb-2" src="assets/img/profile.jpg" alt="" /></span>
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav">
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#about">A propos</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#skills">Compétences</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#experience">Projets</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#awards">Contact</a></li>
+                </ul>
+            </div>
+        </nav>
+        <!-- Contenu -->
+        <div class="container-fluid p-0">
+            <!-- A propos -->
+            <section class="resume-section" id="about">
+                <div class="resume-section-content">
+                    <h1 class="mb-0">
+                        Grégory
+                        <span class="text-info">Jonville</span>
+                    </h1>
+                    <div class="subheading mb-5">
+                        39 Rue Fauchier, 13002 Marseille, France
+                        <a href="mailto:gregory.jonville@laplateforme.io">gregory.jonville@laplateforme.io</a>
+                    </div>
+                    <p class="lead mb-5">Etudiant à la plateforme, Je suis en reconversion dans le monde du développement web.</p>
+                    <div class="social-icons ">
+                        <a href="https://www.linkedin.com/in/gr%C3%A9gory-jonville-4018121b5/"><button type="button" class="btn btn-info btn-icon">
+                            <span class="btn-inner--icon"><i class="fab fa-linkedin"></i></span>
+                            <span class="btn-inner--text">Linkedin</span>
+                        </button></a> <!-- Linkedin -->
+                        <a href="https://github.com/gregory-jonville?tab=overview&from=2020-12-01&to=2020-12-31"><button type="button" class="btn btn-info btn-icon">
+                            <span class="btn-inner--icon"><i class="fab fa-github"></i></span>
+                            <span class="btn-inner--text">GitHub</span>
+                        </button></a> <!-- GITHUB -->
+                        <a href="https://www.facebook.com/greg.gregoryj"><button type="button" class="btn btn-info btn-facebook btn-icon">
+                                <span class="btn-inner--icon"><i class="fab fa-facebook"></i></span>
+                                <span class="btn-inner--text">Facebook</span>
+                            </button></a> <!-- Facebook -->
+                    </div>
+                </div>
+            </section>
+            <hr class="m-0" />
+            <!-- Skills-->
+            <section class="resume-section" id="skills">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Compétences</h2>
+                    <div class="subheading mb-3">Langages de programmation</div>
+                    <h3 class="mb-5">Front-end</h3>
+                    <ul class="list-inline dev-icons">
+                        <li class="list-inline-item"><i class="fab fa-html5"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-css3-alt"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-bootstrap"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-js"></i></li>
+                    </ul>
+                    <h3 class="mb-5">Back-end</h3>
+                    <ul class="list-inline dev-icons">
+                    <li class="list-inline-item"><i class="fab fa-wordpress"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-php"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-mysql"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-ajax"></i></li>
+                        </ul>
+                </div>
+            </section>
+            <hr class="m-0" />
+    <!-- Experience-->
+    
+        </div>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Third party plugin JS-->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
